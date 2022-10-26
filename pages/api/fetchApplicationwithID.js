cb10fde2d098251c7fdaecd5089f6c043f0e97f2
Sounav201 <sounav2001saha@gmail.com>
2022-10-26 12:21:16 +0530

    Added rendering print functionality

---
+++ pages/api/fetchApplicationwithID.js
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import conn from "../../utils/db"
+
+export default async function handler(req, res) {
+  try {
+   const {appID} = req.body;
+
+  const query = `Select * from public.applications where "appid" = '${appID}'`;
+
+  const results = await conn.query(query);
+
+  if(results.rowCount>0)
+  {
+    res.status(200).json({ application: results.rows });
+  
+  }
+  else{
+    res.status(201).json({ message: `No application found having ID ${appID}` });
+  
+  }
+ 
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" ,error:error });
+  }
+  
+}
--- pages/dashboard/previews/[id].js
+++ pages/dashboard/previews/[id].js
@@ -6,13 +6,16 @@ import AppContext from '../../../AppContext';
 import Swal from 'sweetalert2'
 import { useReactToPrint } from 'react-to-print';
 import { render } from 'react-dom';
+import axios from 'axios';
 
-const Preview = () => {
+const Preview = ({data}) => {
     // PDF CODE
     const componentRef = useRef(); // null 
     const handlePrint = useReactToPrint({
       content: () => componentRef.current,
     });
+    const application = data.application[0];
+    const approvalType = application.approvaltype;
 
     const [previewapplicationBody, setpreviewapplicationBody] = useState(null);
     const router = useRouter();
@@ -25,41 +28,12 @@ const Preview = () => {
           if(localStorage.getItem("applicationBody"))
           {
             let previewContent = JSON.parse(localStorage.getItem("applicationBody"));
-            console.log('Preview : ', previewContent , typeof(previewContent));
+           // console.log('Preview : ', previewContent , typeof(previewContent));
             setpreviewapplicationBody(JSON.parse(localStorage.getItem("applicationBody")));
             
           }
         }
       }, [])
-
-    const Sendapplicationhandler =  async () => {
-      //email, appbody, apptype, tempid, userRole
-      //TO DO : Need to delete the local storage variables post successful submission -> applicationBody, applicationVariables, applicationLanguage
-      const email = userEmail || "";
-      const appbody = previewapplicationBody;
-      const apptype = "Sanction Letter";
-      const tempid = "1";
-      const userRole = user || "";
-      const res = await fetch('/api/createApplication', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, appbody, apptype, tempid, userRole }),
-      });
-      const data = await res.json();
-      //console.log(data);
-      if(data.message =="ok")
-      {
-        Swal.fire(
-          'Good job!',
-          'Your application has been sent for review!',
-          'success'
-        );
-      router.push('/dashboard/home');
-      }
-
-    }
 
 
   return (
@@ -73,8 +47,7 @@ const Preview = () => {
         {user === "Executor" && 
               <div className='p-1 flex items-center justify-between my-4  w-5/6 mx-auto '>
               <Button colorScheme="red" onClick={() => router.back()} >Go Back</Button>
-              <Button colorScheme="whatsapp" onClick={handlePrint}>Print</Button>
-              {/* <button className="p-1 bg-green-500" onClick={handlePrint}>Print this out!</button> */}
+        {approvalType && approvalType>4 &&    <Button colorScheme="whatsapp" onClick={handlePrint}>Print</Button> }   
               
           </div>}
         {user.includes("Approver")  && <div className='p-1 flex items-center justify-between my-4  w-5/6 mx-auto '>
@@ -95,5 +68,28 @@ const Preview = () => {
 }
 
 
+export async function getServerSideProps(context) {
+ // const data ={route:context.req.headers.referer}
+  const routeArray = context.req.headers.referer.split("/");
+  const appID = routeArray[routeArray.length-1];
+  var APIendpoint;
+  if (process.env.NODE_ENV === 'development') {
+      APIendpoint = 'http://localhost:3000/api/fetchApplicationwithID'
+  }
+  else if(process.env.NODE_ENV === 'production'){
+      APIendpoint = 'https://templaterepo.vercel.app/api/fetchApplicationwithID';
+  }
 
+ // let res =  await axios.get(APIendpoint);
+//  let data = await res.data;
+
+
+  let response = await axios.post(APIendpoint, {
+    appID: appID,
+  });
+  let data = await response.data;
+  return{
+    props:{data}
+  }
+}
 export default Preview
